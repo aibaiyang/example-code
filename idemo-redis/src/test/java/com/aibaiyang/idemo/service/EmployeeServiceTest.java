b53913083a82f8244eb8db11163ac03711d96d9b
guozhong <15207082658@163.com>
2019-08-16 10:41:36 +0800

    增加idemo-redis项目

--- idemo-redis/src/test/java/com/aibaiyang/idemo/service/EmployeeServiceTest.java
+++ idemo-redis/src/test/java/com/aibaiyang/idemo/service/EmployeeServiceTest.java
@@ -1,15 +1,18 @@
 package com.aibaiyang.idemo.service;
 
 import com.aibaiyang.idemo.entity.Employee;
+import org.assertj.core.util.Lists;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -45,10 +48,13 @@ public class EmployeeServiceTest {
         employee.setId(8)
                 .setCode("by")
                 .setName("by");
-
+        List<Employee> employeeList = Lists.newArrayList(employee);
         ValueOperations<String, Employee> employeeCache = redisTemplate.opsForValue();
         employeeCache.set(employee.getCode(),employee ,10 ,TimeUnit.SECONDS);
         System.out.println(employeeCache.get(employee.getCode()));
+
+        ListOperations<String, List<Employee>> emoloyeeListCache = redisTemplate.opsForList();
+        emoloyeeListCache.set("ss",2L, employeeList);
 
     }
 
