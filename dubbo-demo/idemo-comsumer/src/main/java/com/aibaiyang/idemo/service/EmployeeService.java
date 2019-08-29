80372ac823fd6842fa20cc5feb2f21166d51cf88
guozhong <15207082658@163.com>
2019-08-29 13:02:23 +0800

    添加消费者配置

--- dubbo-demo/idemo-comsumer/src/main/java/com/aibaiyang/idemo/service/EmployeeService.java
+++ dubbo-demo/idemo-comsumer/src/main/java/com/aibaiyang/idemo/service/EmployeeService.java
@@ -1,6 +1,7 @@
 package com.aibaiyang.idemo.service;
 
 import com.aibaiyang.idemo.entity.Employee;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -9,6 +10,7 @@ import java.util.List;
  * @Date 2019/8/14
  * @Description
  **/
+@Service
 public interface EmployeeService {
 
     /**
