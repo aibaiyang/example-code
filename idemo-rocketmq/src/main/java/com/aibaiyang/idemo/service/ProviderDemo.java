a2d13d7a2d87f79b2a1eb205def42abf3d6048fd
aibaiyang <946452613@qq.com>
2019-09-06 00:08:54 +0800

    添加rocketmq-client依赖； 编写发送端简单demo;

---
+++ idemo-rocketmq/src/main/java/com/aibaiyang/idemo/service/ProviderDemo.java
@@ -0,0 +1,42 @@
+package com.aibaiyang.idemo.service;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+
+/**
+ * @Author zhong guo
+ * @Date 2019/9/5 23:45
+ * @description
+ **/
+public class ProviderDemo {
+
+    private static final String NAMESRV_ADDR = "47.70.139.169.9876";
+
+    public static void main(String[] args) throws Exception{
+
+        DefaultMQProducer producer = new DefaultMQProducer("test_quick_producer_name");
+
+        producer.setNamesrvAddr(NAMESRV_ADDR);
+
+        producer.start();
+
+        for (int i = 0; i < 5; i++) {
+
+            //1、创建消息
+            Message message = new Message("test_quick_topic", //主题
+                    "TagA", // 标签
+                     "key" + i, // 唯一标识
+                    ("Hello Rocketmq" + i).getBytes());
+
+            //2、发送消息
+            SendResult sr = producer.send(message);
+            System.out.println("消息发出："+sr);
+
+        }
+
+        producer.shutdown();
+
+    }
+
+}
