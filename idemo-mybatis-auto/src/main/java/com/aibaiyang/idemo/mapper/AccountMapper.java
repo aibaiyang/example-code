bcfa444a003e38f16d7b9e3fb2ab8808d065e06d
aibaiyang <946452613@qq.com>
2019-10-06 12:25:06 +0800

    新建idemo-mybatis-auto项目

---
+++ idemo-mybatis-auto/src/main/java/com/aibaiyang/idemo/mapper/AccountMapper.java
@@ -0,0 +1,7 @@
+package com.aibaiyang.idemo.mapper;
+
+import com.aibaiyang.idemo.entity.Account;
+import com.aibaiyang.idemo.util.MyMapper;
+
+public interface AccountMapper extends MyMapper<Account> {
+}
