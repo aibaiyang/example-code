641f0986b77fb6ed5d37a986193ca7e7afdca32b
003749 <guozhong@fcbox.com>
2020-09-15 17:13:39 +0800

    1、拦截器实现自定义注解；

--- idemo-mybatis/src/main/java/com/aibaiyang/idemo/controller/DemoController.java
+++ idemo-mybatis/src/main/java/com/aibaiyang/idemo/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.aibaiyang.idemo.controller;
 
+import com.aibaiyang.idemo.annotation.LoginRequired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +18,13 @@ public class DemoController {
     public void filterTest(@RequestParam("name") String name){
         System.out.println("filterTest");
     }
+
+
+    @RequestMapping("/loginTest")
+    @LoginRequired
+    public void loginTest(){
+        System.out.println("loginTest");
+    }
+
 
 }
--- idemo-mybatis/src/main/java/com/aibaiyang/idemo/interceptor/LogInterceptor.java
+++ idemo-mybatis/src/main/java/com/aibaiyang/idemo/interceptor/LogInterceptor.java
@@ -1,6 +1,8 @@
 package com.aibaiyang.idemo.interceptor;
 
 
+import com.aibaiyang.idemo.annotation.LoginRequired;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,8 +18,21 @@ public class LogInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println("log");
-        return true;
+
+        System.out.println("进入拦截器了");
+
+        // 反射获取方法上的LoginRequired注解
+        HandlerMethod handlerMethod = (HandlerMethod)handler;
+        LoginRequired loginRequired = handlerMethod.getMethod().getAnnotation(LoginRequired.class);
+        if(loginRequired == null){
+            return true;
+        }
+
+        // 有LoginRequired注解说明需要登录，提示用户登录
+        response.setContentType("application/json; charset=utf-8");
+        response.getWriter().print("你访问的资源需要登录");
+        return false;
+
     }
 
     @Override
